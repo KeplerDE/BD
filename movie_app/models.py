282aea0fde365c06bb6a671bcdd06190d38aeb0f
KeplerDE <90717368+KeplerDE@users.noreply.github.com>
2022-01-25 16:41:43 +0300

    Slug Complete

--- movie_app/models.py
+++ movie_app/models.py
@@ -8,7 +8,7 @@ class Movie(models.Model):
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=10000000)
-    slug = models.SlugField(default='', null=False)
+    slug = models.SlugField(default='', null=False, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -16,7 +16,7 @@ class Movie(models.Model):
 
 
     def get_url(self):
-        return reverse('movie-detail', args=[self.id])
+        return reverse('movie-detail', args=[self.slug])
 
 
 
