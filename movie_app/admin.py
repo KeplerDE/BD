8f473398e2b184bbb2ad6b0852a772a6c51253d1
KeplerDE <90717368+KeplerDE@users.noreply.github.com>
2022-02-11 01:39:12 +0300

    Django TollBar

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['rating', 'name']
+    list_per_page = 3
+
+
+
+admin.site.register(Movie, MovieAdmin)
