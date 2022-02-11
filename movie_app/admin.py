066389eb3353ed461aa07d23ede6b3479613238b
KeplerDE <90717368+KeplerDE@users.noreply.github.com>
2022-02-11 13:56:52 +0300

    Filter Admin Panel

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,6 +4,27 @@ from django.db.models import QuerySet
 
 
 
+class RatingFilter(admin.SimpleListFilter):
+    title = "Фильтр по Рейтингу"
+    parameter_name = "qwerty"
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низкий'),
+            ('от 40 до 59', 'Средний'),
+            ('от 60 до 79', 'Высокий'),
+            ('>=', 'Еще выше'),
+
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value()=='<40':
+            return queryset.filter(rating__lt=40)
+        if self.value()=='от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        return queryset
+
+
 
 
 @admin.register(Movie)
@@ -12,7 +33,9 @@ class MovieAdmin(admin.ModelAdmin):
     list_editable = ['rating', 'currency', 'budget']
     ordering = ['rating', 'name']
     list_per_page = 10
-    actions = ['set_dollars']
+    actions = ['set_dollars', 'set_euro']
+    search_fields = ['name__istartswith']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Status')
     def rating_status(self, mov: Movie):
@@ -26,3 +49,13 @@ class MovieAdmin(admin.ModelAdmin):
     @admin.action(description='Установить валюту доллар')
     def set_dollars(self, request, qs: QuerySet):
         qs.update(currency=Movie.USD)
+
+
+    @admin.action(description='Установить валюту евро')
+    def set_euro(self, request, qs: QuerySet):
+        qs.update(currency=Movie.EUR)
+
+
+
+
+
